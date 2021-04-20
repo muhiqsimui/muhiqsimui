eb34ae87df2fcf5a23de8682d1da607bcddc86a3
Muhammad Iqbal <33776809+muhiqsimui@users.noreply.github.com>
2021-04-21 02:40:37 +0700

    Update test.php

--- assets/test.php
+++ assets/test.php
@@ -1,13 +1,46 @@
 <!DOCTYPE html>
 <html>
+
+
+	<?php 
+	$title="Learn Github tested"
+
+
+	?>
 <head>
-	
+	<title><? echo $title; ?>></title>
+
+	<style type="text/css">
+		body 
+{
+	background-color: black;
+    color:white;
+}
+h1{
+    text-align: center;
+}
+	</style>
+
 
 </head>
 <body>
 
+<div class="heading">
+	<h1 id="demo"></h1>
+</div>
+<div class="header">
+	
+	<div class="navigasi">
+	
+	</div>
+</div>
 
 
-
+<div class="footer">
+	
+</div>
 </body>
+<script>
+document.getElementById("demo").innerHTML = "hi darkness i am light";
+</script>
 </html>
